893f6a197654caf763fbaa3571f8b5a177321af0
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2024-09-18 19:44:52 +0600

    auth

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -50,11 +50,13 @@ export const login = async (req, res) => {
             { expiresIn: age }
         );
 
+        const { password: userPassword, ...userInfo } = user;
+
         res.cookie("token", token, {
             httpOnly: true,
             // secure:true,
             maxAge: age,
-        }).status(200).json({ message: "Login Successfull" });
+        }).status(200).json(userInfo);
     } catch (err) {
         console.log(err);
         res.status(500).json({ message: "Failed to login!" });
