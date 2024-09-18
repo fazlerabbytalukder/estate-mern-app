b896e319e202bb5a6aa918a2436ccdf5402972e8
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2024-09-18 19:05:14 +0600

    register

--- api/app.js
+++ api/app.js
@@ -1,8 +1,10 @@
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import express from "express";
 import authRoute from "./routes/auth.route.js";
 
 const app = express();
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
